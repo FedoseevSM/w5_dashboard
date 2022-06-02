8e26a4e4bb15b05cfed292b428f466708d262318
samgxlden <samgxlden@gmail.com>
2022-06-02 20:51:39 +0300

    v.1.1

--- js/burger.js
+++ js/burger.js
@@ -5,15 +5,19 @@ const sidebar = document.querySelector(".sidebar")
 burgerHeader.addEventListener("click", function () {
   if (sidebar.classList.contains("_burger-active")) {
     sidebar.classList.remove("_burger-active")
+    document.getElementsByTagName("body")[0].style.overflow = "auto"
   } else {
     sidebar.classList.add("_burger-active")
+    document.getElementsByTagName("body")[0].style.overflow = "hidden"
   }
 })
 
 burgerSidebar.addEventListener("click", function () {
   if (sidebar.classList.contains("_burger-active")) {
     sidebar.classList.remove("_burger-active")
+    document.getElementsByTagName("body")[0].style.overflow = "auto"
   } else {
     sidebar.classList.add("_burger-active")
+    document.getElementsByTagName("body")[0].style.overflow = "hidden"
   }
 })
